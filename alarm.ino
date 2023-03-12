4b8bdbc31fb90398f374294d031e03a38603c4f9
JaSy-Panda <127547143+JaSy-Panda@users.noreply.github.com>
2023-03-12 20:31:01 +0100

    Add files via upload

--- alarm.ino
+++ alarm.ino
@@ -1,130 +1,177 @@
 /*============================================================================================================
 Descprition:  Zimmer Guardian
 Author:       Jan Syed
-Date:         05.03.23
-Version:      V0.1.2
+Date:         13.03.2023
+Version:      V1.0.0
 ============================================================================================================*/
 
-#include "stammdaten.h"
-
-#include <Keypad.h>
-const byte COLS = 4; //4 Spalten
-const byte ROWS = 4; //4 Zeilen
-//Die Ziffern und Zeichen des Keypads werden eingegeben:
-char hexaKeys[ROWS][COLS]={
-{'D','#','0','*'},
-{'C','9','8','7'},
-{'B','6','5','4'},
-{'A','3','2','1'}
-};
-
-byte colPins[COLS] = {31,33,35,37}; //Definition der Pins für die 4 Spalten
-byte rowPins[ROWS] = {39,41,43,45};//Definition der Pins für die 4 Zeilen
-char Taste; //Taste ist die Variable für die jeweils gedrückte Taste.
-Keypad Tastenfeld = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); //Das Keypad kann absofort mit "Tastenfeld" angesprochen werden
-
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-long getDistance();
+#include "stammdaten.h" //Stammdaten der Hardware und Variabeln
 
 void setup() {
-  Serial.begin (9600);
-  pinMode(buzzer, OUTPUT);
+
+  pinMode(buzzer, OUTPUT); //Initialisierung Buzzer als Output
+
+  pinMode(g_led, OUTPUT); //Initialisierung Grüne LED als Output
+  pinMode(r_led, OUTPUT); //Initialisierung Rote LED als Output
+
+  //Initialisierung Ultraschallsensor
   pinMode(trigger, OUTPUT);
   pinMode(echo, INPUT);
 
-  pinMode(g_led, OUTPUT);
-  pinMode(r_led, OUTPUT);
-
-  lcd.init();
+  lcd.init(); //Initialisierung LCD
 
 }
 
 void loop() {
 
-  if(!alarm_ein && !alarm_triggered){
+  //Abfrage Zustand der Analge
 
+  if(!alarmOn && !alarmTriggered) // Wenn der Alarm NICHT Scharf ist und auch NICHT Ausgelöst dann:
+  {
 
+    lcd.backlight(); // LCD wird Eingeschaltet
     
-    //digitalWrite(g_led, LOW);
-    //digitalWrite(r_led, LOW);
-    //noTone(buzzer);
-
-    lcd.backlight();
-
+    //Auswahl Menu-LCD
     lcd.setCursor(0, 0);
     lcd.print("A - Alarm Scharf");
-
     lcd.setCursor(0, 1);
-    lcd.print("B - Change PW");
+    lcd.print("B - PW Aendern");
 
-    Taste = Tastenfeld.getKey();
+    Taste = Tastenfeld.getKey(); //User eingabe auf Keypad erkennen
 
-    if(Taste == 'A')
+    //Auswahl User
+    if(Taste == 'A') //Wenn der User die Taste A drückt
     {
       
-      startupAlarm();
-      alarm_ein = true;
+      startupAlarm(); //Alarm wird Scharf gesltellt
+      alarmOn = true; //Alarm ist Scharf
 
     }
-    else if(Taste == 'B' ){
+    else if(Taste == 'B' ) //Wenn der User B drückt
+    {
 
-      //passwordCheck();
+      confirmBeep(); //BEEP für bestätigung das Menupunkt ausgewählt wurde
 
-      
-      String firstPw = getNewPw();
-      passwort = firstPw;
-      
+      //Text: Altes PW Eingeben
+      lcdClearSetCurser();
+      lcd.print("Altes PW ");
+      lcd.setCursor(0, 1);
+      lcd.print("Eingeben");
+      delay(2000);
+
+      passwordCheck(); //Altes Passwort wird überprüft
+      confirmBeep(); //Bestätigungs BEEP
+
+      bool pwCompare = true; //BOOL für den Status des PW-Vergleich
+
+      while(pwCompare) //Führt den Code aus bis der Vergleich richtig ist
+      {
+
+        //Text: Neues PW Eingeben
+        lcdClearSetCurser();
+        lcd.print("Neues PW ");
+        lcd.setCursor(0, 1);
+        lcd.print("Eingeben");
+        delay(2000);
+
+        String firstPw = getNewPw(); //PW wird beim User abgefragt und im String gespeichert
+        confirmBeep(); //Bestätigungs BEEP
+
+        //Text: PW nochmals bestaetigen
+        lcdClearSetCurser();
+        lcd.print("PW nochmals");
+        lcd.setCursor(0, 1);
+        lcd.print("bestaetigen");
+        delay(2000);
+
+        String secondPw = getNewPw();//PW wird nochmals beim User abgefragt und im String gespeichert
+
+        if(firstPw == secondPw) //Wenn die beiden PWs gleich sind
+        {
+          
+          confirmBeep();//Bestätigungs BEEP
+
+          //Text: PW wurde geandert
+          lcdClearSetCurser();
+          lcd.print("PW wurde");
+          lcd.setCursor(0, 1);
+          lcd.print("geaendert");
+          delay(1000);
+          gLedFlashing(); //Grüne LED blinkt als Bestätigung       
+
+          pwCompare = false;//PW sind gleich. Der Vergleich ist abgeschlossen. BOOL wird dafür false
+
+        }
+        else // Wenn die PWs nicht übereinstimmen
+        {
+
+          //Text PW stimmen nicht ueberein
+          lcdClearSetCurser();
+          lcd.print("PW stimmen nicht");
+          lcd.setCursor(0, 1);
+          lcd.print("ueberein");
+
+          //BEEP und Rote LED Blikent als Ablehnung
+          tone(buzzer, 100);
+          digitalWrite(r_led, HIGH);
+          delay(200);
+          noTone(buzzer);
+          digitalWrite(r_led, LOW);
+          delay(3000);
+
+          //Text: Widerholen
+          lcdClearSetCurser();
+          lcd.print("Wiederholen");
+          delay(3000);
+
+          //BOOL wird nicht verändert die Abfrage wiederholt sich
+
+        }
+
+        passwort = firstPw;// Das temporäre PW wird als effektives PW abgespeichert
+
+      }
 
     } 
-    /*else if (Taste == 'D')
-    {
-      
-      lcdflashing();
-
-      passwort = "4321";
-      
-    } */
-
 
   } 
   
-  if (alarm_ein && !alarm_triggered)
+  if (alarmOn && !alarmTriggered) //Wenn der Alarm Scharf ist aber NICHT Ausgelöst dann:
   {
-    Taste = Tastenfeld.getKey();
 
-    int distanz =  getDistance();
+    int distanz =  getDistance(); // Distaz wird gemessen und zwischen gespeichert
 
-    if(distanz < 30)
+    if(distanz < 30) // Überprüfung ab die Distanz näher als 30CM ist
     {
 
-     alarm_triggered = true;
-     lcd.backlight();
+     alarmTriggered = true;// Wenn Ja, wird der status auf Ausgelöst gesezt
+     lcd.backlight(); //Licht des LCD wird wieder eingeschaltet
 
     } 
     
-    if(alarm_ein && alarm_triggered)
+    if(alarmOn && alarmTriggered) //Wenn der Alarm ausgelöst hat
     {
-      digitalWrite(r_led, HIGH);
-      digitalWrite(g_led, LOW);
-      tone(buzzer,800);
+      digitalWrite(r_led, HIGH); //Rote LED wird eingeschaltet
+      digitalWrite(g_led, LOW); // Grüne LED wird ausgeschaltet
+      tone(buzzer,800); //Buzzer wird eingeschaltet
 
-      passwordCheck();
+      passwordCheck(); //Passwort muss richtig eingegeben werden
 
+      //Wenn das PW richtig eingegeben wurde wird der Buzzer und die LED ausgeschaltet
       noTone(buzzer);
       digitalWrite(r_led, LOW);
 
+      //Text: Alarm Deaktiviert
+      lcdClearSetCurser();
+      lcd.print("Alarm Deaktiviert");
 
-
-
-
+      //Bestätigungs BEEP und Blinken Grüne LED
       confirmBeep();
+      gLedFlashing();
 
-
-      alarm_triggered = false;
-      alarm_ein = false;
+      //Der Zustand der Anlage wird zurückgesetzt
+      alarmTriggered = false;
+      alarmOn = false;
 
     }
 
@@ -132,188 +179,187 @@ void loop() {
 
 }
 
-//Start Countdown des Alarms
-
-void startupAlarm(){
-
-  lcd.clear();
-  int countdown = 3;
 
 
-  /*while(countdown != 0)
+void startupAlarm() //Funktion für das Scharfstellen
+{
+
+  lcd.clear(); //LCD Leer
+
+  int countdown = 10;//Countdownlänge für den start. Dauer 10s
+
+
+  while(countdown != 0) //Countdown funktion. Wird wiederholt solange der Countdwon nicht 0 ist.
     {
+      //Text: Alarmscharf in: (Sekunde)
       lcd.setCursor(0, 0);
       lcd.print("Alarm Scharf in:");
-
       lcd.setCursor(0, 1);
       lcd.print(countdown);
       
+      //LED und BEEP fürs Zählen
       digitalWrite(r_led, HIGH);
       tone(buzzer, 300);
-
       delay(250);
-
       digitalWrite(r_led, LOW);
       noTone(buzzer);
-
       delay(750);
 
-      countdown--;
+      countdown--; //Countdown -1
 
-      lcd.clear();
+      lcd.clear(); //Reset LCD
 
+    }
 
+    //Countdown ist Fertig
 
-    } */
-
-  
-  /*tone(buzzer, 300);
-
-  delay(100);
-
-  tone(buzzer, 800);
-  digitalWrite(g_led, HIGH);
-
-  delay(100);
-    
-  noTone(buzzer); */
-
-  digitalWrite(g_led, HIGH);
-  confirmBeep();
-  lcd.noBacklight();
+    digitalWrite(g_led, HIGH); //Grüne LED wird eingeschalter zum signaliesieren das die Anlage Scharf ist
+    confirmBeep(); //Bestätigungs BEEP 
+    lcd.noBacklight(); //LCD wird aus
 
 }
 
-//
 
-long getDistance(){
 
+long getDistance()//Funktion zum ermitteln der Distanz
+{
+
+  //Zeit des ECHO wird gemässen
   digitalWrite(trigger, HIGH);
   delay(10);
   digitalWrite(trigger, LOW);
   dauer = pulseIn(echo, HIGH);
 
+  //Entfernung berechnen
   entfernung = (dauer/2) * 0.03432;
 
-  return entfernung;
+  return entfernung; //Die Entfernung wird zurückgegeben
 
 }
 
-//Funktion
-
-String getNewPw(){
-
-  String eingabe = "";
-  int i = 0; // Variable für die Passwort Länge
-  bool pwInput = true;
-
-  lcdPwEing();
 
 
+String getNewPw() //Funktion für die Abfrage des neuen PWs
+{
 
-  while(pwInput){
+  String eingabe = ""; //String in dem die Eingabe gespeichert wird
+  int i = 0; // Zähler für die PW Länge
+  bool pwInput = true; //BOOL für den Status der Eingabe
+
+  lcdPwEing();//Text: PW Eingeben
+
+
+
+  while(pwInput) //Solange die Eingabe nicht bestätigt wurde. Wird es wiederholt
+  {
     
-    Taste = Tastenfeld.waitForKey();
+    Taste = Tastenfeld.waitForKey(); //Warten bis User eine Taste drückt. Scheibt den input in "Taste"
 
-    if(Taste == '#'){
+    if(Taste == '#') // Wenn die #-Taste gedrückt wird. Wird die Eingabe bestätigt
+    {
 
-      if( i > 3)
+      if( i > 3) //Überprüfung ob das Passwort min 4 Zeichen lang ist
       {
 
-       pwInput = false;
-       return eingabe;
+       pwInput = false; //Wenn Ja abfrage beenden
+       return eingabe; //Gibt das neue PW zurück
 
       }
-      else{
+      else // Wenn das PW kürzer als 4 Zeiche ist
+      {
 
-        lcd.clear();
-        lcd.setCursor(0, 0);
+        //Text: PW ist zu kurz
+        lcdClearSetCurser();
         lcd.print("PW ist zu kurz");
-
         delay(1000);
 
-        lcdPwEing();
+        lcdPwEing(); //Text: PW Eingeben
 
-        eingabe = "";
-        i = 0;
+        eingabe = ""; //die Eingabe wird zurückgesezt 
+        i = 0; //Reset zähler
 
       }
 
     }
-    else if (Taste == '*')
+    else if (Taste == '*') //Wenn die *-Taste gedrückt wird. Wird die Eingabe Zurückgesezt
     {
-      eingabe = "";
-      i = 0;
+      //Die *-Taste dient zum zurücksezten der Eingabe
 
-      lcdPwEing();
+      eingabe = ""; //die Eingabe wird zurückgesezt 
+      i = 0; //Reset zähler
+
+
+      lcdPwEing(); //Text: PW Eingeben
 
     }
-    else if( Taste == 'A' || Taste == 'B' || Taste == 'C'|| Taste == 'D')
+    else if( Taste == 'A' || Taste == 'B' || Taste == 'C'|| Taste == 'D')//Filter damit A, B, C und D nicht als Passwort eingegeben werden können
     {
       //Filter damit A, B, C und D nicht als Passwort eingegeben werden können
     }
-    else if( (Taste) && (i < 8) )
+    else if( (Taste) && (i < maxPwLengh) )//Wenn eine Zahl-taste gedrückt wird und die Länge des PW nicht überschritten wurde
     {
-      eingabe += Taste;
+      eingabe += Taste; //Speicher die Eingabe
 
-      Serial.println(eingabe);
-
+      //Zeig kurz das Eingegebene Passwort auf dem LCD. Danach wird es mit * "versteckt"
       lcd.setCursor(i, 1);
       lcd.print(Taste);
-
-      delay(200);
+      delay(t1);
       lcd.setCursor(i, 1);
       lcd.print("*");
 
-      i++;
+      i++;//Zähler +1
 
     }
 
   }
 
 
+
 }
 
-void passwordCheck(){
+void passwordCheck() // Überprüfung Passwort
+{
 
-  String eingabe = "";
-  int i = 0; // Variable für die Passwort Länge
-  bool pwInput = true;
+  String eingabe = ""; //String in dem die Eingabe gespeichert wird
+  int i = 0; // Zähler für die Anzahl eingaben
+  bool pwInput = true; //BOOL für den Status der Eingabe
 
-  lcdPwEing();
+  lcdPwEing(); //Text: PW Eingebe
 
-  while(pwInput){
+  while(pwInput) //Solange die Eingabe nicht bestätigt wurde. Wird es wiederholt
+  {
     
-    Taste = Tastenfeld.waitForKey();
+    Taste = Tastenfeld.waitForKey(); //Warten bis User eine Taste drückt. Scheibt den input in "Taste"
 
-    if(Taste == '#'){
+    if(Taste == '#')// Wenn die #-Taste gedrückt wird. Wird die Eingabe bestätigt
+    {
 
-      if(eingabe == passwort)
+      if(eingabe == passwort) //Wenn die Eingabe und das definierte PW übereinstimmen
       {
 
-       pwInput = false;
+       pwInput = false; //keine wiederholung mehr
 
       }
-      else{
+      else //Wenn die Eingabe und das definierte PW NICHT übereinstimmen
+      {
 
-        lcd.clear();
-        lcd.setCursor(0, 0);
+        //Text: PW ist Falsch£
+        lcdClearSetCurser();
         lcd.print("PW ist Falsch!");
-
         delay(1000);
 
-        lcdPwEing();
+        lcdPwEing(); //Text: PW Eingeben
 
-        eingabe = "";
-        i = 0;
+        eingabe = ""; //die Eingabe wird zurückgesezt
+        i = 0; //Reset zähler
 
       }
 
     }
-    else if (Taste == '*')
+    else if (Taste == '*') //Wenn die *-Taste gedrückt wird. Wird die Eingabe Zurückgesezt
     {
-      eingabe = "";
-      i = 0;
+      eingabe = ""; //die Eingabe wird zurückgesezt
+      i = 0; //Reset zähler
 
       lcdPwEing();
 
@@ -322,20 +368,18 @@ void passwordCheck(){
     {
       //Filter damit A, B, C und D nicht als Passwort eingegeben werden können
     }
-    else if( (Taste) && (i < 8) )
+    else if( (Taste) && (i < maxPwLengh) ) //Wenn eine Zahl-taste gedrückt wird und die Länge des PW nicht überschritten wurde
     {
-      eingabe += Taste;
+      eingabe += Taste; //Speicher die Eingabe
 
-      Serial.println(eingabe);
-
+      //Zeig kurz das Eingegebene Passwort auf dem LCD. Danach wird es mit * "versteckt"
       lcd.setCursor(i, 1);
       lcd.print(Taste);
-
-      delay(200);
+      delay(t1);
       lcd.setCursor(i, 1);
       lcd.print("*");
 
-      i++;
+      i++; //Zähler +1
 
     }
 
@@ -343,44 +387,54 @@ void passwordCheck(){
 
 }
 
-void confirmBeep(){
 
+
+void confirmBeep() //BEEP-Ton für Bestätigung
+{
+
+  //Duuu - Düü geräusch
   tone(buzzer, 300);
   delay(100);
- 
   tone(buzzer, 800);
   delay(100);
-
   noTone(buzzer);
 
 }
 
-void lcdPwEing(){
+void lcdPwEing() // funktion für den LCD Text: PW Eingeben
+{
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
+  lcdClearSetCurser();
   lcd.print("PW Eingeben");
 
 }
 
-void lcdflashing(){
+void gLedFlashing() //Funktion für das Blinken der Grünen LED
+{
 
-  int countdown = 2;
+  int countdown = 4;
 
   while(countdown != 0)
     {
       
-      delay(300);
-      lcd.noBacklight();
-
-      delay(300);
-      lcd.backlight();
+      delay(250);
+      digitalWrite(g_led, HIGH);
+      delay(250);
+      digitalWrite(g_led, LOW);
 
       countdown--;
 
       lcd.clear();
 
     } 
+
+}
+
+//lcdClearSetCurser breinigt den LCD und setzt den Cursor wierder 0.0. Damit dieser code nicht immer wiederholft geschrieben werden muss
+void lcdClearSetCurser(){
+    
+  lcd.clear();
+  lcd.setCursor(0, 0);
 
 }
 
--- stammdaten.h
+++ stammdaten.h
@@ -1,29 +1,58 @@
 /*============================================================================================================
-Descprition:  Zimmer Guardian
+Descprition:  Stammdaten Zimmer Guardian
 Author:       Jan Syed
-Date:         19.12.2022
-Version:      V0.1.2
+Date:         12.03.2023
+Version:      V1.0.0
 ============================================================================================================*/
 
-int trigger = 13;
-int echo = 12;
+//Variabeln für das Programm
 
-long dauer = 0;
-long entfernung = 0;
+int maxPwLengh = 8; // Variabel für die Maximale Passwortlänge
 
-long distanz;
+const int t1 = 250; //Delay beim Wechseln auf *-Zeichen bei der PW eingaben
 
-const int t1 = 250; //immer ms
+String passwort = "1234"; //standard PW
 
-const int buzzer = 11;
+bool alarmOn = false; // Alarm Scharf status
+bool alarmTriggered = false; // Alarm ausgelöst status
 
-const int g_led = 9;
-const int r_led = 10;
+//Buzzer
+const int buzzer = 11; // Pin 11 für den Buzzer
 
-bool alarm_ein = false;
-bool alarm_triggered = false;
+//LEDs
+const int g_led = 9; //Pin 9 für die Grüne LED
+const int r_led = 10; // Pin 10 für die Rote LED
 
-String passwort = "1234";
+//Ultraschallsensor
+int trigger = 13; //Pin 13 to Trigger
+int echo = 12; //Pin 12 to Echo
+
+long dauer = 0; //Variabel für die Zeitmessung Out / In
+long entfernung = 0; // Variabel für die gemessene Entfernung
+
+long getDistance(); // Initailisierung Funktion für die Messung Distanz
+
+//LCD Hardware
+#include <Wire.h> //Libary für LCD
+#include <LiquidCrystal_I2C.h> //LCD Libary
+LiquidCrystal_I2C lcd(0x27, 16, 2); //LCD format
+
+//Keypad Hardware
+#include <Keypad.h> //Libary für Keypad
+const byte COLS = 4; //4 Spalten
+const byte ROWS = 4; //4 Zeilen
+//Die Ziffern und Zeichen des Keypads werden eingegeben:
+char hexaKeys[ROWS][COLS]={
+{'D','#','0','*'},
+{'C','9','8','7'},
+{'B','6','5','4'},
+{'A','3','2','1'}
+};
+
+byte colPins[COLS] = {31,33,35,37}; //Definition der Pins für die 4 Spalten
+byte rowPins[ROWS] = {39,41,43,45};//Definition der Pins für die 4 Zeilen
+char Taste; //Taste ist die Variable für die jeweils gedrückte Taste.
+Keypad Tastenfeld = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); //Das Keypad kann absofort mit "Tastenfeld" angesprochen werden
 
 
 
