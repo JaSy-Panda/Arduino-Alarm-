0164e798138a6b3043e32605daa83ff95ff04956
JaSy-Panda <127547143+JaSy-Panda@users.noreply.github.com>
2023-03-12 20:16:17 +0100

    Add files via upload

--- alarm.ino
+++ alarm.ino
@@ -1,11 +1,33 @@
 /*============================================================================================================
-Descprition:  Alarm (Zimmer Guardian)
+Descprition:  Zimmer Guardian
 Author:       Jan Syed
-Date:         26.02.2023
-Version:      Alpha
+Date:         27.02.2023
+Version:      V0.1.0
 ============================================================================================================*/
 
 #include "stammdaten.h"
+
+#include <Keypad.h>
+const byte COLS = 4; //4 Spalten
+const byte ROWS = 4; //4 Zeilen
+//Die Ziffern und Zeichen des Keypads werden eingegeben:
+char hexaKeys[ROWS][COLS]={
+{'D','#','0','*'},
+{'C','9','8','7'},
+{'B','6','5','4'},
+{'A','3','2','1'}
+};
+
+byte colPins[COLS] = {31,33,35,37}; //Definition der Pins für die 4 Spalten
+byte rowPins[ROWS] = {39,41,43,45};//Definition der Pins für die 4 Zeilen
+char Taste; //Taste ist die Variable für die jeweils gedrückte Taste.
+Keypad Tastenfeld = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); //Das Keypad kann absofort mit "Tastenfeld" angesprochen werden
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+long getDistance();
 
 void setup() {
   Serial.begin (9600);
@@ -16,94 +38,149 @@ void setup() {
   pinMode(g_led, OUTPUT);
   pinMode(r_led, OUTPUT);
 
-  pinMode(tast1, INPUT);
-  pinMode(tast2, INPUT);
+  lcd.init();
 
 }
 
 void loop() {
+
+  if(!alarm_ein && !alarm_triggered){
+
+
+    lcd.backlight();
+    digitalWrite(g_led, LOW);
+    digitalWrite(r_led, LOW);
+    noTone(buzzer);
+
+    lcd.setCursor(0, 0);
+    lcd.print("A - Alarm Scharf");
+
+    lcd.setCursor(0, 1);
+    lcd.print("B - Change PW");
+
+    Taste = Tastenfeld.getKey();
+
+    if(Taste == 'A')
+    {
+      lcd.clear();
+      startupAlarm();
+
+      alarm_ein = true;
+
+
+    }
+    else if(Taste == 'B' ){
+      
+      digitalWrite(g_led, HIGH);
+      delay(250);
+      digitalWrite(g_led, LOW);
+      Serial.println("okey");
+
+    } 
+
+
+
+  } 
   
+  if (alarm_ein && !alarm_triggered)
+  {
+    Taste = Tastenfeld.getKey();
+
+    int distanz =  getDistance();
+
+    if(distanz < 30)
+    {
+
+     alarm_triggered = true;
+
+    } 
+    else if(Taste == 'B')
+    {
+
+      alarm_ein = false;
+
+    } 
+    
+    if(alarm_ein && alarm_triggered)
+    {
+      digitalWrite(r_led, HIGH);
+      digitalWrite(g_led, LOW);
+      tone(buzzer,800);
+
+    }
+
+
+
+
+  }
+
+  //Serial.println(alarm_ein);
+
+
+
+
+}
+
+//Start Countdown des Alarms
+
+void startupAlarm(){
+
+
+  int countdown = 3;
+
+  /*while(countdown != 0)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print("Alarm Scharf in:");
+
+      lcd.setCursor(0, 1);
+      lcd.print(countdown);
+      
+      digitalWrite(r_led, HIGH);
+      tone(buzzer, 300);
+
+      delay(250);
+
+      digitalWrite(r_led, LOW);
+      noTone(buzzer);
+
+      delay(750);
+
+      countdown--;
+
+      lcd.clear();
+
+
+
+    } */
+
+    tone(buzzer, 300);
+
+    delay(100);
+
+    tone(buzzer, 800);
+    digitalWrite(g_led, HIGH);
+
+    delay(100);
+    
+    noTone(buzzer);
+
+
+}
+
+
+
+long getDistance(){
+
   digitalWrite(trigger, HIGH);
   delay(10);
   digitalWrite(trigger, LOW);
   dauer = pulseIn(echo, HIGH);
 
-  entfern = (dauer/2) * 0.03432;
+  entfernung = (dauer/2) * 0.03432;
 
-  status_tast1=digitalRead(tast1);
-  status_tast2=digitalRead(tast2);
-
-  if(status_tast2 == HIGH)
-  {
-    ein_status = 1;
-  }
-
-
-
-
-  //Check Entfernung
-  if (entfern >= 2000 || entfern <= 0)
-  {
-    Serial.println("Kein Messwert");
-  }
-  else
-  {
-    Serial.println(entfern);
-    Serial.println("cm");
-  }
-
-  //Set Alarm
-  if (entfern <= 30)
-  {
-    alarm_status = 1; 
-  }
-  
-  //Alarm off
-  if (status_tast1 == HIGH && alarm_status == 1)
-  {
-    alarm_status = 0;
-    tone(buzzer, 200);
-    delay(250); 
-    noTone(buzzer);
-
-    for(int i = 0; i < 20; i++)
-    {
-      digitalWrite(r_led, HIGH);
-      delay(175);
-      digitalWrite(r_led, LOW);
-      delay(175);
-    }
-    
-    tone(buzzer, 200);
-    delay(250); 
-    noTone(buzzer);
-    
-
-  }
- 
-
-  if (alarm_status == 1)
-  {
-    digitalWrite(r_led, HIGH);
-    tone(buzzer, 500);
-    delay(175);
-    digitalWrite(r_led, LOW);
-    delay(175);
-
-    digitalWrite(r_led, HIGH);
-    tone(buzzer, 700);
-    delay(175);
-    digitalWrite(r_led, LOW);
-    delay(175);   
-
-  }
-  else {
-
-    digitalWrite(r_led, LOW);
-    noTone(buzzer);
-
-  }
-  
-
+  return entfernung;
 
 }
+
+
--- stammdaten.h
+++ stammdaten.h
@@ -1,28 +1,28 @@
 /*============================================================================================================
-Descprition:  Stammdaten (Zimmer Guardian)
+Descprition:  Stammdaten Zimmer Guardian
 Author:       Jan Syed
-Date:         26.02.2023
-Version:      Alpha
+Date:         27.02.2023
+Version:      V0.1.0
 ============================================================================================================*/
 
 int trigger = 13;
 int echo = 12;
 
 long dauer = 0;
-long entfern = 0;
+long entfernung = 0;
+
+long distanz;
 
 const int t1 = 250; //immer ms
 
-const int buzzer = 10;
+const int buzzer = 11;
 
-const int g_led = 8;
-const int r_led = 9;
+const int g_led = 9;
+const int r_led = 10;
 
-const int tast1 = 9;
-const int tast2 = 10;
+bool alarm_ein = false;
+bool alarm_triggered = false;
 
-int status_tast1 = 0;
-int status_tast2 = 0;
 
-int alarm_status = 0;
-int ein_status = 0;
+
+
